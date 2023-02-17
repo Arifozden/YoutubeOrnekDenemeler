2e21b428e156934191f25ca76c34d56282c0c670
arifozden <arifozden1@gmail.com>
2023-02-17 23:34:02 +0100

    Youtubedan denemeler

---
+++ src/test/java/com/example/variabler1/Variabler1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.variabler1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Variabler1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
